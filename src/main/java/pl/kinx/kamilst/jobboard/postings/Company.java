0c8dbfcbc9ed14821e25d1235cd1e14edfbd08e6
Kinga Michniak <kinga.michniak@outlook.com>
2023-06-07 19:09:02 +0200

    Add Company class

---
+++ src/main/java/pl/kinx/kamilst/jobboard/postings/Company.java
@@ -0,0 +1,11 @@
+package pl.kinx.kamilst.jobboard.postings;
+
+import lombok.Data;
+
+@Data
+public class Company {
+    private String name;
+    private String contact;
+    private String website;
+
+}
