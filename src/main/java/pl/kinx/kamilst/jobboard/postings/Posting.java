4b07095f5b4ed10157fd8d56e383f6bff3aaa6c4
Kinga Michniak <kinga.michniak@outlook.com>
2023-06-07 18:24:33 +0200

    Implement Service and Repository

--- src/main/java/pl/kinx/kamilst/jobboard/postings/Posting.java
+++ src/main/java/pl/kinx/kamilst/jobboard/postings/Posting.java
@@ -2,12 +2,14 @@ package pl.kinx.kamilst.jobboard.postings;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @AllArgsConstructor
 @Getter
+@Setter
 class Posting {
     private Long id;
     private String description;
