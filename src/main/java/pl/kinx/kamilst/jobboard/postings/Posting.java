da6ae69d28b059f5be3521975670a99b7dd99d63
Kinga Michniak <kinga.michniak@outlook.com>
2023-06-07 19:24:40 +0200

    Add validation of requests

--- src/main/java/pl/kinx/kamilst/jobboard/postings/Posting.java
+++ src/main/java/pl/kinx/kamilst/jobboard/postings/Posting.java
@@ -1,9 +1,15 @@
 package pl.kinx.kamilst.jobboard.postings;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -12,8 +18,20 @@ import java.time.LocalDate;
 @Data
 class Posting {
     private Long id;
+
+    @NotNull
+    @Length(min = 1, max = 2560)
     private String description;
+
+    @NotNull
+    @DecimalMin("1.00")
     private BigDecimal salary;
+
+    @Future
+    @NotNull
     private LocalDate expiresAt;
+
+    @NotNull
+    @Valid
     private Company company;
 }
